8e467e833b83fbae9e891e3564a501cb825366d7
FISIDAN <schwark@posteo.de>
2023-01-16 10:44:47 +0100

    Challenge erfüllt Defintion Datentyp

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,15 @@ Berechnen Sie die Summe der beiden Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+
+// Abfrage, Deklaration
+let number01, number02;
+
+number01 = prompt("Bitte erste Zahl eingeben:");
+number02 = prompt("Bitte zweite Zahl eingeben:");
+
+// Berechnung: Datentyp Number ist die mittelung, dass es ssich um eine Zahl handelt, nicht einen string
+let sum = Number(number01) + Number(number02);
+
+//Ausgabe
+console.log("Die Summe der Zahlen ist: " + sum);
